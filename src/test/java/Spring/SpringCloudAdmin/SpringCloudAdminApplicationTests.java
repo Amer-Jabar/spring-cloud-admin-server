10498e50c424971691f5d25dca353de0e8407af3
Amer-Jabar <amerjabar0000@gmail.com>
2022-04-29 16:37:03 +0300

    Initialized repo for spring cloud admin server

---
+++ src/test/java/Spring/SpringCloudAdmin/SpringCloudAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.SpringCloudAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
